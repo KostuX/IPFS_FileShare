9d20434e42d9c8e23d949bd780756037d0fad920
KostuX <77516813+KostuX@users.noreply.github.com>
2025-02-20 21:08:17 +0000

    d

--- app/page.js
+++ app/page.js
@@ -18,7 +18,21 @@ import {
 import SubmitButton from "@/reusable/components/submitButton";
 import openWebSocket from "@/utils/server/ws/wsOpen";
 
+export const IndexContext = createContext();
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [errorMessage, setErrorMessage] = useState({ ok: true, err: [] });
+  const [pageStatus, setPageStatus] = useState(PAGE_STATUS_NORMAL);
+  const [webSocket, setWebSocket] = useState(null);
+  const [socketAddress, setSocketAddress] = useState(null);
+  const [socket, setSocket] = useState(null);
+  const [info, setInfo] = useState([
+    {
+      title: "init",
+      data: "Just Started",
+      time: new Date().toUTCString(),
+    },
+  ]);
   async function addInfoData(newInfoData) {
     if (typeof newInfoData === "string") {
       newInfoData = { data: newInfoData };
@@ -31,40 +45,44 @@ export default function Home() {
     console.log(sessionCookie);
   }, []);
   return (
-    <DefaultLayout>
-      <div className="">
-        <div className=" justify-center w-screen grid">
-          <Logo size={200} />
-          <div className="text-center">File Sharing made easy</div>
-        </div>
+    <IndexContext.Provider
+      value={{
+        input,
+        setInput,
+        errorMessage,
+        setErrorMessage,
+        pageStatus,
+        setPageStatus,
+        webSocket,
+        setWebSocket,
+        socketAddress,
+        setSocketAddress,
+        socket,
+        setSocket,
+        info,
+        setInfo,
+      }}
+    >
+      <DefaultLayout>
+        <div className="">
+          <div className=" justify-center w-screen grid">
+            <Logo size={200} />
+            <div className="text-center">File Sharing made easy</div>
+          </div>
 
-        <div className="justify-center w-screen grid">
-          <InputComponent
-            prop={{ setInput, setErrorMessage, setSocketAddress }}
-          />
-        </div>
-        <div className="justify-center w-screen grid mt-4">
-          <SubmitButton
-            prop={{
-              pageStatus,
-              setPageStatus,
-              setErrorMessage,
-              input,
-              setSocketAddress,
-              addInfoData,
-              setSocket,
-              socket,
-            }}
-          >
-            Submit
-          </SubmitButton>
-        </div>
-        <div className="mt-4 w-1/2">
-          <ErrorMessage prop={{ errorMessage }} />
-        </div>
+          <div className="justify-center w-screen grid">
+            <InputComponent
+              prop={{ setInput, setErrorMessage, setSocketAddress }}
+            />
+          </div>
+          <div className="justify-center w-screen grid mt-4">
+            <SubmitButton>Submit</SubmitButton>
+          </div>
+          <div className="mt-4 w-1/2">{/* <ErrorMessage /> */}</div>
 
-        <InfoWindow prop={{ info }} />
-      </div>
-    </DefaultLayout>
+          <InfoWindow prop={{ info }} />
+        </div>
+      </DefaultLayout>
+    </IndexContext.Provider>
   );
 }
